50eebbdd1e764218ae9fbcf587b936570ede7f21
linuxcat <sarndeepnijjar@googlemail.com>
2017-10-23 13:30:02 +0100

    trying text instead of symbols

--- spec/dockerapp/docker_compose_spec.rb
+++ spec/dockerapp/docker_compose_spec.rb
@@ -2,13 +2,13 @@ require 'dockerspec/serverspec'
 
 describe docker_compose('docker-compose.yml', wait: 10) do
 
-  its_container(:redis) do
+  its_container('redis') do
     describe port(6379) do
       it { should be_listening('tcp') }
     end
   end
 
-  its_container(:dockerapp) do
+  its_container('dockerapp') do
     describe package('Flask') do
       it { should be_installed.by('pip').with_version('0.11.1') }
     end
