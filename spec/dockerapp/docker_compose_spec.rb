3e87e5875ee15af733b4a44e45e44324d5cb1fb7
linuxcat <sarndeepnijjar@googlemail.com>
2017-10-23 22:34:27 +0100

    more tests to develop

--- spec/dockerapp/docker_compose_spec.rb
+++ spec/dockerapp/docker_compose_spec.rb
@@ -1,16 +1,34 @@
 require 'dockerspec/serverspec'
+require 'redis'
 
-describe docker_compose('docker-compose.yml', wait: 10) do
+
+describe docker_compose('docker-compose.yml', wait: 20, rm: true) do
 
   its_container(:redis) do
+
     describe port(6379) do
       it { should be_listening('tcp') }
     end
+
   end
 
   its_container(:dockerapp) do
+
+    describe user('admin') do
+      it {should exist}
+      it {should have_home_directory('/home/admin')}
+      it {should have_login_shell('/bin/bash')}
+    end
+
     describe package('Flask') do
       it { should be_installed.by('pip').with_version('0.11.1') }
+    end
+    describe package('redis') do
+      it { should be_installed.by('pip').with_version('2.10.5') }
+    end
+
+    describe service('app.py') do
+      it {should be_running}
     end
   end
 
